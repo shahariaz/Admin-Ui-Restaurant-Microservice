ff15d29a87470ebc9befa36cb146d010352d8689
shahariaz <shahariaz.info@gmail.com>
2025-06-27 01:14:36 +0600

    feat:update dashbord

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,84 @@
-import { Typography } from "antd";
+import {
+  Button,
+  Card,
+  Col,
+  List,
+  Row,
+  Skeleton,
+  Space,
+  Statistic,
+  Tag,
+  Typography,
+} from "antd";
+import Icon from "@ant-design/icons";
 import { useAuthStore } from "../../store";
-import { useEffect, useState } from "react";
-const { Title } = Typography;
+
+import { BarChartIcon } from "../../components/icons/BarChart";
+import {} from "../../components/icons/BasketIcon";
+import { Link } from "react-router-dom";
+import { ComponentType, useEffect, useState } from "react";
+import BasketIcon from "../../components/icons/BasketIcon";
+const { Title, Text } = Typography;
+
+const list = [
+  {
+    OrderSummary: "Peperoni, Margarita ...",
+    address: "Bandra, Mumbai",
+    amount: 1200,
+    status: "preparing",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+];
+
+interface CardTitleProps {
+  title: string;
+  PrefixIcon: ComponentType<unknown>;
+}
+
+const CardTitle = ({ title, PrefixIcon }: CardTitleProps) => {
+  return (
+    <Space>
+      <Icon component={PrefixIcon} />
+      {title}
+    </Space>
+  );
+};
+
 export const HomePage = () => {
   const { user } = useAuthStore();
   const [greeting, setGreeting] = useState("");
@@ -29,8 +106,74 @@ export const HomePage = () => {
   return (
     <div>
       <Title level={4}>
-        {greeting} , {user?.firstName}😒
+        {greeting}, {user?.firstName} 😀
       </Title>
+      <Row className='mt-4' gutter={16}>
+        <Col span={12}>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+              <Card bordered={false}>
+                <Statistic title='Total orders' value={52} />
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card bordered={false}>
+                <Statistic
+                  title='Total sale'
+                  value={70000}
+                  precision={2}
+                  prefix='₹'
+                />
+              </Card>
+            </Col>
+            <Col span={24}>
+              <Card
+                title={<CardTitle title='Sales' PrefixIcon={BarChartIcon} />}
+                bordered={false}
+              ></Card>
+            </Col>
+          </Row>
+        </Col>
+        <Col span={12}>
+          <Card
+            bordered={false}
+            title={<CardTitle title='Recent orders' PrefixIcon={BasketIcon} />}
+          >
+            <List
+              className='demo-loadmore-list'
+              loading={false}
+              itemLayout='horizontal'
+              loadMore={true}
+              dataSource={list}
+              renderItem={(item) => (
+                <List.Item>
+                  <Skeleton avatar title={false} loading={item.loading} active>
+                    <List.Item.Meta
+                      title={
+                        <a href='https://ant.design'>{item.OrderSummary}</a>
+                      }
+                      description={item.address}
+                    />
+                    <Row style={{ flex: 1 }} justify='space-between'>
+                      <Col>
+                        <Text strong>₹{item.amount}</Text>
+                      </Col>
+                      <Col>
+                        <Tag color='volcano'>{item.status}</Tag>
+                      </Col>
+                    </Row>
+                  </Skeleton>
+                </List.Item>
+              )}
+            />
+            <div style={{ marginTop: 20 }}>
+              <Button type='link'>
+                <Link to='/orders'>See all orders</Link>
+              </Button>
+            </div>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
