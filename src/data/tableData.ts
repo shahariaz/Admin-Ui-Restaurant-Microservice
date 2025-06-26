0934419fb8b736ec79f6162cad8921f3645fcb4e
shahariaz <shahariaz.info@gmail.com>
2025-06-26 23:37:24 +0600

    add table data

---
+++ src/data/tableData.ts
@@ -0,0 +1,44 @@
+export const list = [
+  {
+    OrderSummary: "Peperoni, Margarita ...",
+    address: "Bandra, Mumbai",
+    amount: 1200,
+    status: "preparing",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+];
